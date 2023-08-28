bcfd9ca80aded82a902df094eb3c2a07845e16aa
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2023-08-28 15:28:58 +0000

    Deploying to gh-pages from @ Marixa82/goit-react-hw-05-movies@13f08ab68ec8c8ff0a48c91a4acc243b823a07bc 🚀

--- static/js/681.c9974925.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{9681:function(e,r,t){t.r(r),t.d(r,{Reviews:function(){return s}});var n=t(184),a=t(1087).useParams,s=function(){var e=a().movieId;return(0,n.jsxs)("div",{children:["Review: ",e]})}}}]);
-//# sourceMappingURL=681.c9974925.chunk.js.map
